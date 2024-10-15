c0e4cefa5a15c59065af924a527dd927d6406adb
vktor28 <167107545+vktor28@users.noreply.github.com>
2024-10-15 21:16:56 +0200

    Add files via upload

---
+++ sorteo_tiendas/docu/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"App"},{"p":"<Unnamed>","l":"Clientes"},{"p":"<Unnamed>","l":"Informatica"},{"p":"<Unnamed>","l":"Papeleria"}];updateSearchResults();
